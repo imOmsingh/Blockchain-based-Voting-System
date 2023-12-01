c6a64b8172a3d86178d44cfe5bdf401eecb9d4b8
imOmsingh <omsingh200310@gmail.com>
2023-12-01 16:55:16 +0530

    first commit

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ paddingBottom: 20, paddingTop: 50, textAlign: "center" }}
+      className="text-normal"
+    >
+      <div>Mode with ❤️ Manpreet Singh &nbsp;| Om Singh &nbsp;| Goraksh Verma</div>
+    </footer>
+  );
+};
+
+export default Footer;
